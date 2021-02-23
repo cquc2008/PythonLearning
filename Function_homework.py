aeb171718e4eef10719fd9ba94490860d284a4a8
cquc2008 <44984043+cquc2008@users.noreply.github.com>
2021-02-23 11:26:12 -0500

    Update Function_homework.py

--- Function_homework.py
+++ Function_homework.py
@@ -1,3 +1,17 @@
-def get_sum(*args):
-    for i in args:
+# 1. 编写函数，求1+2+3+...+N 的和
+def get_sum(n):
+    sum = 0
+    for i in range(0,n+1):
+        sum += i
+    return sum
+
+def get_sum(n):
+    sum = 0
+    i = 1
+    while i <= n:
+        sum += i
+        i += 1
+    return sum
+
+# 2. 
         
